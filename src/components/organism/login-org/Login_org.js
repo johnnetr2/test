9f86aebc71265f099cb891ab0b8a99de12c86268
Rashdan Natiq <rashdan@natiq.se>
2022-02-08 19:24:33 +0500

    responsiveness login signup

--- src/components/organism/login-org/Login_org.js
+++ src/components/organism/login-org/Login_org.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+<<<<<<< Updated upstream
 import { Container, Box, Typography } from '@mui/material';
+=======
+import { makeStyles } from '@material-ui/core/styles'
+import { Container, Box } from '@mui/material'
+>>>>>>> Stashed changes
 import Label_field from '../../../components/molecule/label-field/Label_field';
 import Filled_btn from '../../../components/atom/filled-btn/Filled_btn';
 import Outline_btn from '../../../components/atom/outline-btn/Outline_btn';
@@ -8,6 +13,7 @@ import './Login_org.css'
 
 const Login_org = () => {
     return (
+<<<<<<< Updated upstream
         <Container sx={{margin:0, padding:0, boxSizing:'border-box', display:'flex'}}>
             <Container sx={{height:'100vh', width:'50%', backgroundColor:'#0A1596'}}></Container>
             <Container sx={{width:'50%', margin:5}} >
@@ -24,6 +30,39 @@ const Login_org = () => {
             </Box>
             </Container>
         </Container>
+=======
+        <>
+            <Container style={{ display: 'flex', margin: 0, padding: 0, width: '100vw', height: '100vh' }}>
+                <Container style={{ display: 'flex', backgroundColor: '#0A1596', margin: 0, padding: 0, width: '50vw' }}></Container>
+                <Container style={{ display: 'flex', margin: 0, padding: 50, width: '50vw', boxSizing: 'border-box' }}>
+                    <div className="login-org-1">
+                        <div className="inner-login-org">
+                            <h1>Logga in</h1>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+                            <Label_field
+                                name="email"
+                                type='text'
+                                onChange={getVal}
+                                value={user.email}
+                                title="Email" />
+                            <Label_field
+                                type='password'
+                                onChange={getVal}
+                                name="password"
+                                value={user.password}
+                                title="Password" />
+                            <u>Glomt losenord?</u>
+                            <Link to="/dashboard"><Filled_btn onClick={loginFunc} title="Logga in" /></Link>
+                            <p>eller</p>
+                            <Outline_btn title="Logga in Med Google" />
+                            <p className="last-para-1">Har du ingte konto? <u>Skapa konto har</u></p>
+                        </div>
+                    </div>
+                </Container>
+            </Container>
+
+        </>
+>>>>>>> Stashed changes
     )
 }
 
--- src/components/organism/signup-org/Signup_org.js
+++ src/components/organism/signup-org/Signup_org.js
@@ -2,13 +2,14 @@ import React from 'react'
 import './Signup_org.css'
 import { Container, Typography, Box } from '@mui/material'
 import { Link } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
+import { Container, Box } from '@mui/material'
 import Label_field from '../../../components/molecule/label-field/Label_field';
 import Filled_btn from '../../../components/atom/filled-btn/Filled_btn';
 import Outline_btn from '../../../components/atom/outline-btn/Outline_btn';
 
-
-
 const Signup_org = () => {
+<<<<<<< Updated upstream
     return (
         <Container sx={{ margin: 0, padding: 0, boxSizing: 'border-box', display: 'flex' }}>
             <Container sx={{ width: '50%', backgroundColor: '#0A1596' }}></Container>
@@ -28,6 +29,120 @@ const Signup_org = () => {
                 </Box>
             </Container>
         </Container>
+=======
+
+    const [register, setRegister] = useState(
+        {
+            fullName: "",
+            email: "",
+            password: ""
+        })
+
+    const changeHandler = (e) => {
+        const { name, value } = e.target;
+        setRegister({ ...register, [name]: value })
+        console.log(register, "this is the console of the change Handler")
+    }
+
+    const clickHandler = (e) => {
+        e.preventDefault()
+        const data = {
+            fullName: register.fullName,
+            email: register.email,
+            password: register.password
+        }
+
+        const URL = EndPoints.SignUp
+        instance.post(URL, data).then(response => {
+            console.log(response.data.user, 'this is the api response')
+            if (response.data.message == 'success') {
+                if (response.data.user.token) {
+                    swal("Success!", response.data.message, 'success')
+                    window.location.href = "/login"
+                }
+            }
+            else {
+                swal("Warning!", response.data.message)
+            }
+        }).catch((error) => {
+            console.log(error);
+        })
+
+    }
+
+    const useStyle = makeStyles({
+        root: {
+            margin: 0,
+            padding: 0,
+            boxSizing: 'border-box'
+        },
+        last_para:
+        {
+            lineHeight:'.75rem',
+            marginTop:'20px',
+            textTransform:'uppercase',
+            width:'90%',
+            textAlign:'center',
+            fontSize:'.75rem'
+        },
+        second_last_para:
+        {
+            lineHeight:'2.75rem',
+            marginTop:'20px',
+            textTransform:'uppercase',
+            width:'90%',
+            textAlign:'center',
+            fontSize:'.75rem'
+        }
+    });
+
+    const classes = useStyle()
+
+    return (
+        <>
+            <Container style={{ display: 'flex', margin: 0, padding: 0, width: '100%', height:'fit-content' }} >
+                <Container style={{ display: 'flex', border:'1px solid #888', backgroundColor: '#0A1596', margin: 0, padding: 0, width: '50vw' }} >
+                    {/* <Box elevation={2} style={{height:'50vh', width:'50vh', border:'1px solid #212121', backgroundColor:'red'}}></Box> */}
+                </Container>
+                <Container style={{ display: 'flex', margin: 0, padding: 100, width: '50vw' }} >
+                    <div className="signup-org-1">
+
+                        <div className="inner-signup-org-1">
+                            <h1>Registrering</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            <Label_field
+                                placeholder="Enter Full Name"
+                                type='text'
+                                onChange={changeHandler}
+                                value={register.fullName}
+                                name="fullName"
+                                title="Full Name" />
+                            <Label_field
+                                placeholder="Enter Email"
+                                type='email'
+                                onChange={changeHandler}
+                                value={register.email}
+                                name="email"
+                                title="Email" />
+                            <Label_field
+                                placeholder="Enter Password"
+                                type='password'
+                                onChange={changeHandler}
+                                value={register.password}
+                                name="password"
+                                title="Password" />
+                            <u>Glomt losenord?</u>
+                            <Filled_btn title="Skapa konto" onClick={clickHandler} />
+                            <p>eller</p>
+                            <Outline_btn title="Konto med Google" />
+                            <p className={classes.second_last_para}>Har du redan ett konto?<Link to="/login">Logga in</Link></p>
+                            <p className={classes.last_para}>This site is protected by recaptcha and the Google privacy policy and terms of service apply</p>
+                        </div>
+                    </div>
+                </Container>
+            </Container>
+        </>
+>>>>>>> Stashed changes
     )
 }
 
