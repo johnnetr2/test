b310eeead9f1411fbae7760e2e1ef2196e87075f
Rashdan Natiq <rashdan@natiq.se>
2022-03-04 19:58:34 +0500

    store and start quiz

--- src/components/atom/OutlineField/OutlineField.js
+++ src/components/atom/OutlineField/OutlineField.js
@@ -1,31 +1,30 @@
 import React from 'react'
-import {Box, Checkbox, label} from '@mui/material';
+import { Box, Checkbox, label } from '@mui/material';
 
 
 const Outline_Field = (props) => {
     return (
-        <div onChange={(e) => props.onChange(e.target.checked)}>
-            {/* <input className='outline-field-input' type={props?.type} />{props.title} */}
+        <div onClick={(e) => props.onClickCheck(e.target.checked)}>
             <Box
-         sx={{
-            width: 140,
-            height: 60,
-            backgroundColor: '#fff',
-            boxShadow:'1px 1px 8px #dfdfdf',
-            borderRadius:'.25rem',
-            marginLeft:'.25rem',
-            marginRight:'.25rem',
-            border:'1px solid #e1e1e1',
-            display:'flex',
-            flexWrap:'wrap',
-            color:'#555555',
-            justifyContent:'center',
-            alignItems:'center',
-            }}
+                sx={{
+                    width: 140,
+                    height: 60,
+                    backgroundColor: '#fff',
+                    boxShadow: '1px 1px 8px #dfdfdf',
+                    borderRadius: '.25rem',
+                    marginLeft: '.25rem',
+                    marginRight: '.25rem',
+                    border: '1px solid #e1e1e1',
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    color: '#555555',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
             >
-                {props.type == 'checkbox' && <Checkbox size="small" sx={{ margin: '0rem', width: 20, height: 20, }} m={0} />}
+                <Checkbox checked={props.checked} size="small" sx={{ margin: '0rem', width: 20, height: 20}} m={0} />
                 {props.title}
-        </Box>
+            </Box>
         </div>
     )
 }
