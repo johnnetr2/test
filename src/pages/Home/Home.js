482c7134f9d0b568bf4b0ffc00b768a6f6556731
Rashdan Natiq <rashdan@natiq.se>
2022-04-02 10:28:46 +0500

    fix crash quiz not found

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -16,13 +16,13 @@ const Home = () => {
     // userId: '61d44d9ca94c4e0398b040da'
     userId: localStorage.getItem('userId'),
   })
-  useEffect(() => {
-    if (params?.state?.popUpStatus) {
-      setFirstPopup(false);
-    } else {
-      setFirstPopup(true);
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (params?.state?.hidepopup) {
+  //     setFirstPopup(false);
+  //   } else {
+  //     setFirstPopup(true);
+  //   }
+  // }, []);
 
   const data = {
     attemptDate: collection.date,
