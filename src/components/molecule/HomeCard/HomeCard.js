232afb797c3774020e2490d099eb19651b9e9c6c
Rashdan Natiq <rashdan@natiq.se>
2022-08-20 22:16:13 +0500

    change route

--- src/components/molecule/HomeCard/HomeCard.js
+++ src/components/molecule/HomeCard/HomeCard.js
@@ -75,7 +75,6 @@ const HomeCard = (props) => {
         <Typography variant="body2" sx={{ fontSize: "0.75rem" }}>
           {data?.information}
         </Typography>
-        {console.log((props?.previousRecord.CorrectQuestion / props?.previousRecord.AttemptedQuestion) * 100, 'consoleeee',)}
         <Box>
           <ProgressBar average={props?.previousRecord ? ((props?.previousRecord.CorrectQuestion / props?.previousRecord.AttemptedQuestion) * 100 ?
 
