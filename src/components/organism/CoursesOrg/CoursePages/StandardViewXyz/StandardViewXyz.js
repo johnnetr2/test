b306f6760683151acd7eca51077444844c5c4496
Alexander Jernström <46424392+AlexanderJernstrom@users.noreply.github.com>
2022-10-24 16:17:56 +0200

    Made back button go to last viewed question

--- src/components/organism/CoursesOrg/CoursePages/StandardViewXyz/StandardViewXyz.js
+++ src/components/organism/CoursesOrg/CoursePages/StandardViewXyz/StandardViewXyz.js
@@ -55,9 +55,10 @@ const StandardViewXyz = () => {
     if (params?.state?.questionIndex != undefined) {
       console.log(params.state.quiz.simuleraQuiz);
       setTime(params?.state?.timeLeft);
-      setQuiz(params?.state?.quiz.simuleraQuiz);
+      setQuiz(params?.state?.quiz);
       setCurrentIndex(params?.state?.questionIndex);
       setStatus(true);
+      setOpen(false)
     } else {
       const URL = EndPoints.getSimuleraQuiz + params.state.id;
       console.log(URL);
@@ -80,6 +81,7 @@ const StandardViewXyz = () => {
           simuleraQuiz: quiz?._id,
           simuleraSeason: quiz?.season,
           timeLeft,
+          currentQuestion: currentIndex, 
         },
       });
     }
