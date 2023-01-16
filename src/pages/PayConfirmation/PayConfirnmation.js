ae2076cb84ee0a2ccd47a4fc622e74561498f2b9
rashdan natiq <rashdan@natiq.se>
2023-01-16 19:55:44 +0500

    complete payment flow

--- src/pages/PayConfirmation/PayConfirnmation.js
+++ src/pages/PayConfirmation/PayConfirnmation.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import CustomModal from "./Modal";
+import { EndPoints, instance2 } from "../../components/service/Route";
+import swal from "sweetalert";
 
 const PayConfirmation = () => {
   const order_id = localStorage.getItem("order_id");
@@ -10,10 +12,28 @@ const PayConfirmation = () => {
   const completionCheck = () => {
     if (order_id) {
       //Make a GET request to our API to see if this order_id is completed. Set isComplete to true
+
+      instance2.get(EndPoints.makePremium).then(res => {
+        if (res.data.status === 200) {
+          setIsComplete(true)
+        } else {
+          swal("Error", "Something went wrong!", "error")
+        }
+      }).catch(err => {
+        console.log("error", err)
+        swal("Error", "Something went wrong!", "error")
+
+      })
     } else {
+      swal("Error", "Something went wrong!", "error")
       //Handle error and set isComplete to false
     }
   };
+
+  useEffect(() => {
+    completionCheck()
+  }, [])
+
 
   const navigate = useNavigate();
 
