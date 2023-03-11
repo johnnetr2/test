7faae050568a7f20b65d9114f7ed9c21a2f9c2aa
Rashdan Natiq <rashdan@natiq.se>
2023-03-11 18:04:13 +0500

    fixes in the ui for premium

--- src/components/molecule/HomeCard/HomeCard.js
+++ src/components/molecule/HomeCard/HomeCard.js
@@ -49,22 +49,6 @@ const HomeCard = (props) => {
 
   return (
     <Box
-      sx={{
-        height: "20%",
-        maxWidth: { md: "unset", lg: "41rem" },
-        display: "flex",
-        justifyContent: "space-between",
-        border: "1px solid #e1e1e1",
-        boxShadow: "0px 5px 10px #f2f2f2",
-        padding: 3,
-        marginTop: 3,
-        marginBottom: 3,
-        borderRadius: 1,
-        "&:hover": {
-          cursor: "pointer",
-          backgroundColor: "#f9f9f9",
-        },
-      }}
       onClick={() => {
         if (data.title !== "XYZ" && !props?.isPremium) {
           // TEMPORARY
@@ -77,97 +61,126 @@ const HomeCard = (props) => {
           });
         }
       }}
-    >
-      <Box sx={{ width: "60%" }}>
-        {(!props?.isPremium && data.title !== "XYZ") && (
-          <Box
-            sx={{
-              borderRadius: "0px 4px 0px 0px",
-              textAlign: "center",
-              width: "70px",
-              height: "23px",
-              backgroundColor: "#FFE482",
-              color: appColors.blackColor,
-              position: "relative",
-              right: "-560px",
-              top: "-24px",
-              margin: "0px",
-              padding: "0px",
-            }}
-          >
-            PREMIUM
-          </Box>
-        )}
-        <Typography variant="h5">{data.title}</Typography>
-        <Typography variant="body2" sx={{ fontSize: "0.75rem" }}>
-          {data?.information}
-        </Typography>
-        <Box>
-          {!props.isLoading ? (
-            <ProgressBarLoader />
-          ) : (
-            <ProgressBar
-              average={
-                props?.previousRecord
-                  ? (props?.previousRecord.totalCorrectQuestion /
-                      props?.previousRecord.totalQuestionPerCategory) *
-                    100
-                  : 0
-              }
-            />
-          )}
-        </Box>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "20%",
-        }}
-      >
-        <Typography variant="h4" style={{ paddingRight: ".75rem" }}>
-          {props?.previousRecord?.totalAttemptedHundred >= 20
-            ? percentageCalculation()
-            : "-"}
-        </Typography>
-
+      sx={{
+        height: "20%",
+        maxWidth: { md: "unset", lg: "41rem" },
+        display: "flex",
+        flexDirection: "column",
+        border: "1px solid #e1e1e1",
+        boxShadow: "0px 5px 10px #f2f2f2",
+        marginTop: 3,
+        marginBottom: 3,
+        borderRadius: 1,
+        "&:hover": {
+          cursor: "pointer",
+          backgroundColor: "#f9f9f9",
+        },
+      }}>
+      {(!props?.isPremium && data.title !== "XYZ") && (
         <Box
           sx={{
             display: "flex",
-            flexDirection: "column",
-            width: "3.5rem",
+            justifyContent: "flex-end",
+            width: "100%",
           }}
         >
-          {props?.previousRecord?.totalAttemptedHundred < 20 && (
-            <CustomizedTooltip
-              title="Gör minst 20 frågor på tid för att få poängprognos"
-              placement="top"
-            >
-              <img
-                src={informationIcon}
-                style={{
-                  display: "flex",
-                  height: "14px",
-                  width: "14px",
-                  alignSelf: "flex-end",
-                }}
-                alt="info"
+          <span style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            padding: '0px 3px',
+            borderRadius: "0px 4px 0px 0px",
+            textAlign: "center",
+            width: "80px",
+            // height: "23px",
+            backgroundColor: "#FFE482",
+            color: appColors.blackColor
+          }}>
+            PREMIUM
+          </span>
+        </Box>
+      )}
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          padding: (!props?.isPremium && data.title !== "XYZ") ? '0px 24px 24px' : 3,
+        }}
+
+      >
+        <Box sx={{ width: "60%" }}>
+
+          <Typography variant="h5">{data.title}</Typography>
+          <Typography variant="body2" sx={{ fontSize: "0.75rem" }}>
+            {data?.information}
+          </Typography>
+          <Box>
+            {!props.isLoading ? (
+              <ProgressBarLoader />
+            ) : (
+              <ProgressBar
+                average={
+                  props?.previousRecord
+                    ? (props?.previousRecord.totalCorrectQuestion /
+                      props?.previousRecord.totalQuestionPerCategory) *
+                    100
+                    : 0
+                }
               />
-            </CustomizedTooltip>
-          )}
-          <Typography
-            variant="body1"
-            style={{
-              fontSize: ".75rem",
-              marginTop: ".75rem",
+            )}
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "20%",
+          }}
+        >
+          <Typography variant="h4" style={{ paddingRight: ".75rem" }}>
+            {props?.previousRecord?.totalAttemptedHundred >= 20
+              ? percentageCalculation()
+              : "-"}
+          </Typography>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "3.5rem",
             }}
           >
-            Prognos
-          </Typography>
+            {props?.previousRecord?.totalAttemptedHundred < 20 && (
+              <CustomizedTooltip
+                title="Gör minst 20 frågor på tid för att få poängprognos"
+                placement="top"
+              >
+                <img
+                  src={informationIcon}
+                  style={{
+                    display: "flex",
+                    height: "14px",
+                    width: "14px",
+                    alignSelf: "flex-end",
+                  }}
+                  alt="info"
+                />
+              </CustomizedTooltip>
+            )}
+            <Typography
+              variant="body1"
+              style={{
+                fontSize: ".75rem",
+                marginTop: ".75rem",
+              }}
+            >
+              Prognos
+            </Typography>
+          </Box>
         </Box>
       </Box>
     </Box>
+
   );
 };
 
